8327011d9c2d3d3802f0cffbfbf8cccd027b9f78
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2026-01-17 05:37:49 -0500

    Change import paths to relative in page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
-import Header from '@/components/Header';
-import Hero from '@/components/Hero';
-import FAQ from '@/components/FAQ';
-import Footer from '@/components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import FAQ from './components/FAQ';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
